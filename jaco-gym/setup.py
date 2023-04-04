1ccf133d72a224df8ec91fea7be869f9d959dcbe
Pranav Rajbhandari <prajbhan@bardot.autonomy.ri.cmu.edu>
2023-04-04 18:28:32 -0400

    eqtup

--- jaco-gym/setup.py
+++ jaco-gym/setup.py
@@ -2,5 +2,13 @@ from setuptools import setup
 
 setup(name='jaco_gym',
       version='0.0.1',
-      install_requires=['gym']
+      install_requires=['gym==0.15.7',
+          'scipy>=1.1.0',
+          'rospkg',
+          'box2d', 
+          'box2d-kengz', 
+          'pyyaml', 
+          'optuna', 
+          'pytablewriter',
+          'tensorflow-gpu==1.14']
 )
