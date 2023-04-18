4d57097bc3c4171fbfbf18cc9886a00c641217dd
Pranav Rajbhandari <prajbhan@cagney.autonomy.ri.cmu.edu>
2023-04-18 17:24:42 -0400

    added randomness

--- jaco-gym/scripts/camera_sample.py
+++ jaco-gym/scripts/camera_sample.py
@@ -20,12 +20,30 @@ save_dir=os.path.join('img_data','simulation')
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
-wait=.5
+wait=.3
 points=np.load(aim)
+finger_samples=3
+noise_samples=10
+noise_bounds=(  (-5,5), #random movement of each joint chosen from these bounds , put 0,0 for none
+                (-5,5),
+                (-5,5),
+                (-5,5),
+                (-5,5),
+                (-180,180),) # this is the rotation, can do whatever
 
 for p in points:
-    env.move_arm(p)
-    rospy.sleep(.5)
-    t=str(time.time()).replace('.','_') # unique name
-    env.save_image(os.path.join(save_dir,t+'.jpeg'))
+    for _ in range(noise_samples):
+        loc=[]
+        for i in range(len(p)):
+            l,h=noise_bounds[i]
+            noise=np.random.random()*(h-l)+l
+            loc.append(p[i]+noise)
+        env.move_arm(loc)
+        
+        for _ in range(finger_samples): # pinch/unpinch fingers a random amount
+            fingy_pos=np.random.random()
+            env.move_fingy(fingy_pos) 
+            rospy.sleep(wait)
+            t=str(time.time()).replace('.','_') # unique name
+            env.save_image(os.path.join(save_dir,t+'.jpeg'))
     
