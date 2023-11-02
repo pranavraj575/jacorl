64399e33c8fc2d8191f1fe925b780f34835ab60d
Your Name <you@example.com>
2023-11-02 16:31:11 -0400

    tweaks

--- jaco-gym/jaco_gym/envs/gazebo_env.py
+++ jaco-gym/jaco_gym/envs/gazebo_env.py
@@ -63,7 +63,7 @@ class JacoGazeboEnv(JacoEnv):
                 (pose.orientation.x,pose.orientation.y,pose.orientation.z,pose.orientation.w)=stuff
             self.spawn_model(name, xml_text, "", pose, "world")
             self.models_spawned.add(name)
-            rospy.sleep(.01)
+            rospy.sleep(.1)
         else:
             print("WARNING: attempt to spawn model with existing name: "+name)
     def spawn_model_from_name(self,model_name,name,position,orientation=None):
@@ -83,7 +83,7 @@ class JacoGazeboEnv(JacoEnv):
         if model_name in self.models_spawned:
             self.delete_model(model_name)
             self.models_spawned.remove(model_name)
-            rospy.sleep(.01)
+            rospy.sleep(.1)
         else:
             print("WARNING: attempt to despawn non-existant model")
     def despawn_all(self):
@@ -103,6 +103,6 @@ class JacoGazeboEnv(JacoEnv):
         model_state_msg.pose = pose
         model_state_msg.reference_frame = "world"
         self.pub.publish(model_state_msg)
-        rospy.sleep(.01)
+        rospy.sleep(.1)
 
     
